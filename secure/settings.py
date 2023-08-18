bdcb556eb7f4e22cef93811837e88cf95edf6dc9
indisecure <67788369+indisecure@users.noreply.github.com>
2023-08-18 15:04:30 +0530

    Update settings.py

--- secure/settings.py
+++ secure/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ed#-0jbmt6jbw=+my&&h1m2g-5kvqt(jt4)xq*6w-!y^rsg68q
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.vercel.app','.now.sh']
 
 
 # Application definition
