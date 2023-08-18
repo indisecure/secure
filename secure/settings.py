075a04f3ae9bcc162d646ed24f04092ab129829b
indisecure <67788369+indisecure@users.noreply.github.com>
2023-08-18 15:00:28 +0530

    Update settings.py

--- secure/settings.py
+++ secure/settings.py
@@ -115,10 +115,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
-
+import os
 STATIC_URL = 'static/'
-STATICFILES_DIRS=['static']
-STATIC_ROOT='staticfiles_build'
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
