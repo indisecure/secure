9a9476e53b27410c03f4234d253223d526c3b686
indisecure <67788369+indisecure@users.noreply.github.com>
2023-08-19 17:57:56 +0530

    Update settings.py

--- secure/settings.py
+++ secure/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 MEDIA_URL = 'media/'
 STATICFILES_DIRS = os.path.join(BASE_DIR, 'my_static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'django_static', 'my_static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'my_static')
 
 
 # Default primary key field type
